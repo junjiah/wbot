cd22da5710e470dcccb341e15ed18ebc49bb9420
Junjia He <edfward@gmail.com>
2016-08-21 00:25:11 -0700

    Only use two drivers for killer

--- zombie-killer/killer.py
+++ zombie-killer/killer.py
@@ -1,4 +1,5 @@
 # coding=utf-8
+import logging
 import pickle
 import re
 
@@ -6,46 +7,88 @@ from contextlib import contextmanager
 
 from selenium import webdriver
 
-# Load cookies.
+# Logging.
+logging.basicConfig(filename='killer.log',level=logging.INFO)
+
+# Load cookies and configure phantomjs.
 cookies = pickle.load(open("cookies.pkl", "rb"))
 cookies_str = ';'.join('%s=%s' % (name, val) for name, val in cookies.items())
-
-# Followers page at first.
-url = "http://weibo.cn/3205389480/fans?rightmod=1&wvr=6"
+cap = {
+    'phantomjs.page.settings.resourceTimeout': 1000,
+    'phantomjs.page.settings.loadImages': False,
+    'phantomjs.page.settings.disk-cache': True,
+    'phantomjs.page.customHeaders.Cookie': cookies_str,
+}
+for k, v in cap.iteritems():
+    webdriver.DesiredCapabilities.PHANTOMJS[k] = v
 
 @contextmanager
-def get_new_page(url):
-
-    def get_driver():
-        cap = webdriver.DesiredCapabilities.PHANTOMJS
-        cap["phantomjs.page.settings.resourceTimeout"] = 1000
-        cap["phantomjs.page.settings.loadImages"] = False
-        cap["phantomjs.page.settings.disk-cache"] = True
-        cap["phantomjs.page.customHeaders.Cookie"] = cookies_str
-
-        driver = webdriver.PhantomJS(
-            executable_path="./phantomjs", desired_capabilities=cap)
-        driver.set_window_size(1120, 1000000)
-        return driver
-
-    driver = get_driver()
-    driver.get(url)
+def get_driver():
+    driver = webdriver.PhantomJS(
+        executable_path="./phantomjs")
+    driver.set_window_size(1120, 1000000)
     yield driver
     driver.quit()
 
-for _ in range(30):  # TODO: first 30 pages.
-    with get_new_page(url) as follower_page:
-        links = follower_page.find_elements_by_link_text(u'移除')
-        for link in links:
-            href = link.get_attribute('href')
-            uid = re.findall('uid=([0-9]*)', href)
-            if uid:
-                uid = uid[0]
-            if uid:
-                with get_new_page(href) as confirm_remove_page:
-                    l = confirm_remove_page.find_element_by_link_text(u'确定')
-                    with get_new_page(l.get_attribute('href')) as remove_page:
-                        print 'Deleted ' + uid
+def construct_remove_url(uid, st):
+    return 'http://weibo.cn/attention/remove?act=removec&uid={}&st={}' \
+        .format(uid, st)
 
-        next_page = follower_page.find_element_by_link_text(u'下页')
-        url = next_page.get_attribute('href')
+class RemoveZombieException(Exception):
+    pass
+
+def remove_zombies_in_one_page(pager, remover):
+    remove_links = pager.find_elements_by_link_text(u'移除')
+    if not remove_links:
+        raise RemoveZombieException('No Remove links')
+
+    # Get mysterious 'st'.
+    link = remove_links[0]
+    href = link.get_attribute('href')
+    st = re.findall('st=(.*$)', href)
+    if not st:
+        raise RemoveZombieException('Mysterious "st"" not found in link')
+    st = st[0]
+
+    for link in remove_links:
+        href = link.get_attribute('href')
+        uid = re.findall('uid=([0-9]*)', href)
+        if not uid:
+            msg = 'UID not found in link: ' + href
+            logging.warning(msg)
+            continue
+        uid = uid[0]
+        remove_url = construct_remove_url(uid, st)
+        remover.get(remove_url)
+        logging.info('Deleted ' + uid)
+
+
+if __name__ == '__main__':
+    follower_url = "http://weibo.cn/3205389480/fans?rightmod=1&wvr=6"
+
+    PAGE_NUM = 10
+    FIND_NEXT_RETRY = 3
+
+    with get_driver() as pager, get_driver() as remover:
+        pager.get(follower_url)
+
+        for i in range(PAGE_NUM):
+            try:
+                remove_zombies_in_one_page(pager, remover)
+            except RemoveZombieException as e:
+                logging.exception(str(e))
+
+            for _ in range(FIND_NEXT_RETRY):
+                try:
+                    next_page = pager.find_element_by_link_text(u'下页')
+                    pager.get(next_page.get_attribute('href'))
+                    logging.info('Go to next page: %d' % (i + 1))
+                    break
+                except Exception as e:
+                    logging.exception(str(e))
+                    logger.info('Retry after a second')
+                    time.sleep(1)
+            else:
+                msg = 'Failed to find next page for %d times, exit' \
+                    % FIND_NEXT_RETRY
+                raise Exception(msg)
