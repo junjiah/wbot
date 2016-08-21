40dad62943f6650791763389a20e9bb777c3f4aa
Junjia He <edfward@gmail.com>
2016-08-21 00:48:55 -0700

    Delete followers in a page in parallel

--- zombie-killer/killer.py
+++ zombie-killer/killer.py
@@ -5,6 +5,7 @@ import re
 
 from contextlib import contextmanager
 
+from joblib import Parallel, delayed
 from selenium import webdriver
 
 # Logging.
@@ -34,10 +35,19 @@ def construct_remove_url(uid, st):
     return 'http://weibo.cn/attention/remove?act=removec&uid={}&st={}' \
         .format(uid, st)
 
+def remove_zombie(remove_url, uid):
+    with get_driver() as remover:
+        try:
+            remover.get(remove_url)
+        except Exception as e:
+            return 'Failed to remove %s, because of: %s' % (uid, str(e))
+        else:
+            return 'Deleted %s' % uid
+
 class RemoveZombieException(Exception):
     pass
 
-def remove_zombies_in_one_page(pager, remover):
+def remove_zombies_in_one_page(pager):
     remove_links = pager.find_elements_by_link_text(u'移除')
     if not remove_links:
         raise RemoveZombieException('No Remove links')
@@ -47,9 +57,10 @@ def remove_zombies_in_one_page(pager, remover):
     href = link.get_attribute('href')
     st = re.findall('st=(.*$)', href)
     if not st:
-        raise RemoveZombieException('Mysterious "st"" not found in link')
+        raise RemoveZombieException('Mysterious "st" not found in link')
     st = st[0]
 
+    remove_urls, uids = [], []
     for link in remove_links:
         href = link.get_attribute('href')
         uid = re.findall('uid=([0-9]*)', href)
@@ -58,9 +69,13 @@ def remove_zombies_in_one_page(pager, remover):
             logging.warning(msg)
             continue
         uid = uid[0]
-        remove_url = construct_remove_url(uid, st)
-        remover.get(remove_url)
-        logging.info('Deleted ' + uid)
+        uids.append(uid)
+        remove_urls.append(construct_remove_url(uid, st))
+
+    res = Parallel(n_jobs=len(uids))(
+        delayed(remove_zombie)(url, uid) for url, uid in zip(remove_urls, uids))
+    for msg in res:
+        logging.info(msg)
 
 
 if __name__ == '__main__':
@@ -69,12 +84,12 @@ if __name__ == '__main__':
     PAGE_NUM = 10
     FIND_NEXT_RETRY = 3
 
-    with get_driver() as pager, get_driver() as remover:
+    with get_driver() as pager:
         pager.get(follower_url)
 
         for i in range(PAGE_NUM):
             try:
-                remove_zombies_in_one_page(pager, remover)
+                remove_zombies_in_one_page(pager)
             except RemoveZombieException as e:
                 logging.exception(str(e))
 
@@ -82,12 +97,13 @@ if __name__ == '__main__':
                 try:
                     next_page = pager.find_element_by_link_text(u'下页')
                     pager.get(next_page.get_attribute('href'))
-                    logging.info('Go to next page: %d' % (i + 1))
-                    break
                 except Exception as e:
                     logging.exception(str(e))
                     logger.info('Retry after a second')
                     time.sleep(1)
+                else:
+                    logging.info('Go to next page: %d' % (i + 1))
+                    break
             else:
                 msg = 'Failed to find next page for %d times, exit' \
                     % FIND_NEXT_RETRY
